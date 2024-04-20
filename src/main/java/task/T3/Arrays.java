2dd085fc79c644bdf4d0c81542169d8af3dbe3b7
BuildTools <unconfigured@null.spigotmc.org>
2024-04-20 09:18:53 +0300

    New class Arrays

---
+++ src/main/java/task/T3/Arrays.java
@@ -0,0 +1,21 @@
+package task.T3;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+    }
+    public static int func1(int[] numbers) {
+
+        return 0;
+    }
+
+    public static void func2(int[] numbers) {
+
+    }
+
+    public static void func3(int[] numbers) {
+
+    }
+
+}
+
