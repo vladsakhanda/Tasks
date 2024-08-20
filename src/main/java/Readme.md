f1ac0bf167d3b654c38f185eba62bde59eb8534c
“Vladyslav <unconfigured@null.spigotmc.org>
2024-08-20 11:50:20 +0300

    Added Readme.md

---
+++ src/main/java/Readme.md
@@ -0,0 +1,103 @@
+# Header1
+## Header2
+## Header3
+## Header4
+<hr>
+
+*italic text text*
+
+_italic text text_
+<hr>
+
+
+**bold**
+
+__bold__
+<hr>
+
+<p style="color:red">asdasd</p>
+
+<style>fjksdhfjksd</style>
+<hr>
+
+~~Переклеслювання~~
+
+***Курсив + жирний***
+
+Нормальний текст
+
+<hr>
+
+Це мій <sub>маленький</sub> текст
+
+Це мій <sup>маленький</sup> текст
+
+`Текст текст`
+<hr>
+
+А потім важлива людина сказала:
+> Сторони Усика та Ф'юрі вже узгодили майбутній реванш. 
+> Опцію на бій-відповідь зможуть активувати обидва боксери.
+
+> Він не погодився
+
+<hr>
+
+You should use new `brush`!!!
+
+<hr>
+
+Посилання на сайт [тут](https://suspilne.media)
+
+<a href="https://jsfiddle.net/5mvaw0zr/12">wwwww</a>
+
+[Перейти в папку java (бо Readme.md знаходиться в java)]()
+
+[Перейти в task](Task)
+
+[Перейти в task](Task/T6)
+
+
+![sdfsdf](../resources/download.jpg)
+
+- asd
+- asd
+- asd
+
+1. a
+2. 2e
+3. 43
+
+
+1. asas
+- fsd
+- fsdf
+2. fsdgfsdf
+
+- sdfsf
+    - gdfg
+    - fsdgfdg
+      - sdf
+        - sdfsdf
+          - sdfsdf
+            - adsfsdf
+                - sdfsf
+                  - gdfg
+                    - fsdgfdg
+                        - sdf
+                            - sdfsdf
+                                - sdfsdf
+                                    - adsfsdf
+
+
+
+
+
+
+<hr>
+
+> [!NOTE]
+> fsdfsdfsfgsdg
+
+> [!WARNING]
+> fsdfsdfsfgsdg
--- src/main/java/task/T14/Shop.java
+++ src/main/java/task/T14/Shop.java
@@ -1,29 +1,35 @@
 package task.T14;
 
-public class Shop {
-    public static void main(String[] args) throws Exception {
-        try {
-            books(6);
-        } catch (Exception e) {
-            int b = books.length;
-            int c = b - 1;
+import java.util.Scanner;
 
-            System.out.println("Такої книжки не існує, є книжки тільки з номером від 0 до " + c + " спробуйте ще раз");
+public class Shop {
+    static String[] books = {"b1", "b2", "b3", "b4"};
+
+    public static void main(String[] args) throws Exception {
+
+        /*
+        while () {
         }
 
+        do {
 
+        } while ();
+         */
+        do {
+            try {
+                books(new Scanner(System.in).nextInt());
+            } catch (Exception e) {
+                int lastIndex = books.length - 1;
+
+                System.out.printf("Такої книжки не існує, є книжки тільки з номером від 0 до %d спробуйте ще раз", lastIndex);
+            }
+            System.out.println("1 - закінчити програму, 2 - спробувати ще раз");
+        }
+        while (new Scanner(System.in).next().equals("2"));
     }
-   static String[] books = {"b1","b2","b3","b4"};
 
-
-
-   static void books (int a) throws Exception {
-       int b = books.length;
-           if (a > b) {
-               throw new Exception();
-           } else {
-               System.out.println(books[a]);
-           }
-       }
+    static void books(int a) throws Exception {
+        System.out.println(books[a]);
     }
 }
+
