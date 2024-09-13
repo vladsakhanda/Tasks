28dfb2e84f55ec985ef56db558f18134752ab104
“Vladyslav <unconfigured@null.spigotmc.org>
2024-09-13 21:12:42 +0300

    Added Readme.md

---
+++ src/main/java/task/T129/Animal.java
@@ -0,0 +1,19 @@
+package task.T129;
+
+public class Animal {
+    void eat() {
+
+    }
+}
+
+class Cat extends Animal {
+    void meow() {
+
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+
+    }
+}
---
+++ src/main/java/task/T129/Main.java
@@ -0,0 +1,7 @@
+package task.T129;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
